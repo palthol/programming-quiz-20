2166959c15c56c2a0d7813aeaab2633b23f4ff50
palthol <ilikericeat@gmail.com>
2025-04-01 17:16:46 -0400

    small optimizations. Added lang attribute to html. Changed logical OR operator to a Nullish calescing operator because i hate myself

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express';
-// import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT ?? 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
